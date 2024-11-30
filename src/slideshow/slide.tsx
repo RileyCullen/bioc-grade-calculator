8535c484a092f7e3b26be47f1de04d3ef393878d
Riley Cullen <rccullen.cullen@gmail.com>
2024-11-29 21:10:48 -0800

    Slideshow and Slide work

--- src/slideshow/slide.tsx
+++ src/slideshow/slide.tsx
@@ -1,3 +1,4 @@
+import { Container, Grid2 } from '@mui/material';
 import { ReactNode } from 'react';
 
 export interface SlideProps {
@@ -7,9 +8,18 @@ export interface SlideProps {
 function Slide(props: SlideProps) {
     const { children } = props;
     return (
-        <>
-            { children }
-        </>
+        <Container maxWidth='md' sx={{ border: '1px solid blue' }}>
+            <Grid2
+                container
+                direction='row'
+                justifyContent='center'
+                alignItems='center'
+            >
+                <>
+                    { children }
+                </>
+            </Grid2>
+        </Container>
     );
 }
 
--- src/slideshow/slideshow.tsx
+++ src/slideshow/slideshow.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react';
+import { Button, Container, Grid2 } from '@mui/material';
+import React, { ReactNode, useState } from 'react';
 
 interface SlideshowProps {
     children: ReactNode;
@@ -7,10 +8,31 @@ interface SlideshowProps {
 export function Slideshow(props: SlideshowProps) {
     const { children } = props;
     const slides = React.Children.toArray(children);
+    const [ currentSlide, setCurrentSlide ] = useState(0);
 
     return (
-        <>
-            { slides[0] }
-        </>
+        <Container maxWidth='lg' sx={{ border: '1px solid red'}}>
+            <Grid2
+                container
+                direction='row'
+                justifyContent='center'
+                alignItems='center'
+                spacing={0}
+            >
+                <Button
+                    disabled={currentSlide === 0}
+                    onClick={() => setCurrentSlide(currentSlide - 1)}
+                >
+                    Back
+                </Button>
+                {slides[currentSlide]}
+                <Button
+                    disabled={currentSlide === (slides.length - 1)}
+                    onClick={() => setCurrentSlide(currentSlide + 1)}
+                >
+                    Next
+                </Button>
+            </Grid2>
+        </Container>
     )
 }
